7fb9b0f410223f913cd133f992b8a57c225cf9c6
agmorrow <morrow.austin5@gmail.com>
2022-08-24 22:31:38 -0600

    view work button fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,13 +11,12 @@ const Hero = () => {
         <div className='max-w-[525px] 2xl:max-w-[600px] mx-auto px-20 pt-20 flex flex-col justify-center h-full'>
           <img className='opacity-90 rounded-md' src={Logo} alt='Morrow Web Dev Logo' />
           <div>
-            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center rounded-md hover:bg-amber-400 hover:border-amber-400'>
-              <Link to="work" smooth={true} duration={500} >
+            <Link to="work" smooth={true} duration={500} >
+              <button className='group text-white group border-2 px-6 py-3 my-2 flex items-center rounded-md hover:bg-amber-400 hover:border-amber-400'>
                 View Work
-              </Link>
-              <span className='group-hover:rotate-90 duration-300'>
-                <HiArrowNarrowRight className='ml-3' />
-              </span></button>
+                <span className='group-hover:rotate-90 duration-300'> <HiArrowNarrowRight className='ml-3' /></span>
+              </button>
+            </Link>
           </div>
         </div>
       </section>
